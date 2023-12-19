9e881fdaba1a83f90fee18954dac1541bf66101c
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2023-12-19 17:02:45 +0100

    idea gitignore, defines route and routesDAO

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import org.example.dao.RoutesDAO;
+import org.example.entities.Route;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW-TEAM1-BE");
+
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+        RoutesDAO routesDAO= new RoutesDAO(em);
+        Route route = new Route("ommmm", "nope", 2010.0,1000.0);
+        routesDAO.save(route);
+        routesDAO.findRoutesByArrivalLocation("ov").forEach(System.out::println);
     }
 }
