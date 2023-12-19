ca89f3f26e98402f29376e9dea17ef28b4d67509
SarahGuarneri <sarahguarneri97@icloud.com>
2023-12-19 17:24:17 +0100

    User&Seller classes and DAO

---
+++ src/main/java/org/example/dao/Seller.java
@@ -0,0 +1,77 @@
+package org.example.dao;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+public class Seller {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    @Column(name = "punto_di_emissione")
+    private String puntoDiEmissione;
+    @Column(name = "seller_type")
+    private SellerType sellerType;
+    @Column(name = "service_type")
+    private Service serviceType;
+    @OneToMany(mappedBy = "seller")
+    @Column(name = "control_managment_list")
+    private List<ControlManagement> controlManagementList;
+
+    public Seller() {}
+
+    public Seller(String puntoDiEmissione, SellerType sellerType, Service serviceType, List<ControlManagement> controlManagementList) {
+        this.puntoDiEmissione = puntoDiEmissione;
+        this.sellerType = sellerType;
+        this.serviceType = serviceType;
+        this.controlManagementList = controlManagementList;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getPuntoDiEmissione() {
+        return puntoDiEmissione;
+    }
+
+    public void setPuntoDiEmissione(String puntoDiEmissione) {
+        this.puntoDiEmissione = puntoDiEmissione;
+    }
+
+    public SellerType getSellerType() {
+        return sellerType;
+    }
+
+    public void setSellerType(SellerType sellerType) {
+        this.sellerType = sellerType;
+    }
+
+    public Service getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(Service serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public List<ControlManagement> getControlManagementList() {
+        return controlManagementList;
+    }
+
+    public void setControlManagementList(List<ControlManagement> controlManagementList) {
+        this.controlManagementList = controlManagementList;
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "id=" + id +
+                ", puntoDiEmissione='" + puntoDiEmissione + '\'' +
+                ", sellerType=" + sellerType +
+                ", serviceType=" + serviceType +
+                ", controlManagementList=" + controlManagementList +
+                '}';
+    }
+}
+
---
+++ src/main/java/org/example/dao/User.java
@@ -0,0 +1,82 @@
+package org.example.dao;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID card;
+    private String name;
+    private String surname;
+    private int age;
+    @OneToOne(mappedBy = "user")
+    private Pass pass;
+    @OneToMany(mappedBy = "user")
+    @Column(name = "ticket_list")
+    private List<Ticket> ticketList;
+
+    public User() {
+    }
+
+    public User(String name, String surname, int age, Pass pass, List<Ticket> ticketList) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.pass = pass;
+        this.ticketList = ticketList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Pass getPass() {
+        return pass;
+    }
+
+    public void setPass(Pass pass) {
+        this.pass = pass;
+    }
+
+    public List<Ticket> getTicketList() {
+        return ticketList;
+    }
+
+    public void setTicketList(List<Ticket> ticketList) {
+        this.ticketList = ticketList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "card=" + card +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", pass=" + pass +
+                ", ticketList=" + ticketList +
+                '}';
+    }
+}
